e5482be9177b688bd4df8fbc6951e1dcfe2a9755
Odin <odin.chiang@gmail.com>
2020-12-23 16:07:55 +0800

    day003 complete

--- day003.py
+++ day003.py
@@ -6,7 +6,226 @@ height = int(input("What is your height in cm? "))
 # = Assignment
 # == Check Equality
 
+bill = 0
 if height >= 120:
     print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age < 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+        
+    wantsPhoto = input("Do you want a photo taken? Y or N. ")
+    if wantsPhoto == "Y":
+        bill += 3
+        
+    print(f"Your final bill is ${bill}")
 else:
     print("Sorry, you have to gro taller before you can ride.")
+
+print("=======================")
+
+# Example - Odd or Even
+number = int(input("Which number do you want to check? "))
+isEven = number % 2 == 0 # Modulo or Modulus 模數：取餘數
+if isEven:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
+print("=======================")
+
+# Example - BMI Calculator 2.0
+height = float(input("What is your height in m? "))
+weight = float(input("What is your weight in kg? "))
+bmi = round(weight / (height ** 2))
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+
+print("=======================")
+
+# Example - Leap Year 閏年
+# on every year that is evenly divisible by 4
+    # **except** every year that is evenly divisible by 100
+        #**unless** the year is also evenly divisible by 400
+# 1. 如果年份被 4 整除，則移至步驟 2。 否則，請跳至步驟 5。
+# 2. 如果年份被 100 整除，則移至步驟 3。 否則，請跳至步驟 4。
+# 3. 如果年份被 400 整除，則移至步驟 4。 否則，請跳至步驟 5。
+# 4. 該年份為閏年 (有 366 天)。
+# 5. 該年分不是閏年 (有 365 天)。
+# 參考流程圖：https://app.diagrams.net/?lightbox=1&target=blank&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Leap%20Algorithm#R7VpNc9owEP01HNuxJdmYY4CkzTTNdEpmmhyFrdhqhcXIIkB%2BfSUsY4wcQhqwC%2B0p0urDu29Xu08KHTiYLD4JPE2%2B8oiwDnCiRQcOOwC4CPjqj5Ysc0k3gLkgFjQyk0rBiD4TI3SMdEYjklUmSs6ZpNOqMORpSkJZkWEh%2BLw67ZGz6lenOCaWYBRiZkt/0EgmuTQA3VL%2BmdA4Kb7s%2Br18ZIKLycaSLMERn2%2BI4GUHDgTnMm9NFgPCNHgFLvm6qxdG14oJksp9FoDbYTJlQFxPomj0Jbzr4tT9YJTN5LIwmETKftPlQiY85ilml6W0L/gsjYje1VG9cs4N51MldJXwJ5FyaZyJZ5IrUSInzIwqhcXyXq//6BXdB7PdqjNcVHpL08t11Qq%2BCIERZXwmQrLD7iKUsIiJ3DEPrB2lIpzwCVH6qHWCMCzpU1UPbEItXs8rvaEaxiFvcI7Z9wmzmfnS6O7i%2B53lstIhGt15QiUZTfHK/rk6lVXwzZ5ESLLYDaNttlkAYE85brVoWZxWE%2BTz8oy4xRFONs4Hco6ElfePBjLYM5DhOwN5tfRCCLzcmDDlNJXZxs7ftKAMFLcLtgKlyOulq/M9S8evlfvzWPD/x8LOWHCdQwSD5W3kdre97W8d91w1s64Mg7fGFQLWl5qIK2Dl4%2BtMr9GahYzglGkjOsBnCv3%2BWKhWrFsRfaIZHas4A85YT0EdeGVn8YRPxrOsmQxe8Jcif3dr8jf07PwdHCt/QwvbW65xvSHK/HcVvEfK2IAzLlZr4WMQkjBU8kwK/otsjIwDD3nOYQB23SrA65PQWoHs1gBsAasMllX0MKNxqtqhMpwooPoaFqr48YUZmNAoyjMoyegzHq%2B20iiaQ6z29fodb6j3Ukkzy/PngeLYDSwm0rOBhjU4g2PhHFg4P5Ds5IGG/j5AoyaBdmErZX5B5X1RyFX7oaz4qlfWeN0pSvxfQA2KVPM6N2j3xhO06VP3XH2KWvWpcyjapG%2BZLRMn6IPX63pdvTkacXJtVnpwzhR5JIhQHWcKwBj6/oFIqVfFFtaR0kY5E2jnVeFESwzaMx2BVtMRaOd14ERLzL4%2BdXutlhh0qBKD2i8xyNum2rWJsI5rH6/I9M6lyHi9bXRRzUWm4TJjM6RzuDOi3hZXClq%2BMRYmnNcbCHS3I7oO6EbfQIDNSc8AaGQBDdt%2BbAL2s%2Bk5ZA47Sdch3WzusCnGST9QQ9RkHVTd8ocO%2Bb9jyp%2BLwMvf
+
+year = int(input("Which year do you want to check? "))
+isBy4 = year % 4 == 0
+isBy100 = year % 100 == 0
+isBy400 = year % 400 == 0
+if isBy4:
+    if not isBy100:
+        print("Leap year.")
+    elif isBy400:
+        print("Leap year.")
+    else:
+        print("Not leap year.")
+else:
+    print("Not leap year.")
+
+print("=======================")
+
+#Example - Pizza Order
+# Small Pizza: $15
+# Small Pizza: $15
+# Medium Pizza: $20
+# Medium Pizza: $20
+# Large Pizza: $25
+# Large Pizza: $25
+# Pepperoni for Small Pizza: +$2
+# Pepperoni for Small Pizza: +$2
+# Pepperoni for Medium or Large Pizza: +$3
+# Pepperoni for Medium or Large Pizza: +$3
+# Extra cheese for any size pizza: + $1
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+isPepperoni = input("Do you want pepperoni? Y or N ")
+isExtraCheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+if size == "S":
+    bill = 15
+elif size == "M":
+    bill = 20
+elif size == "L":
+    bill = 25
+else:
+    print("Error!")
+    
+if isPepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+    
+if isExtraCheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}")
+
+print("=======================")
+
+# Example - Love Calculator
+'''
+You are going to write a program that tests the compatibility between two people.
+To work out the love score between two people:
+Take both people's names and check for the number of times the letters in the word TRUE occurs. Then check for the number of times the letters in the word LOVE occurs. Then combine these numbers to make a 2 digit number.
+For Love Scores less than 10 or greater than 90, the message should be:
+"Your score is **x**, you go together like coke and mentos."
+For Love Scores between 40 and 50, the message should be:
+"Your score is **y**, you are alright together."
+Otherwise, the message will just be their score. e.g.:
+"Your score is **z**."
+
+e.g.
+name1 = "Angela Yu"
+name2 = "Jack Bauer"
+
+T occurs 0 times
+R occurs 1 time
+U occurs 2 times
+E occurs 2 times
+
+Total = 5
+
+L occurs 1 time
+O occurs 0 times
+V occurs 0 times
+E occurs 2 times
+
+Total = 3
+
+Love Score = 53
+
+Print: "Your score is 53."
+'''
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+# 名字轉成小寫串接在一起
+nameLower = name1.lower() + name2.lower()
+# TRUE 的個數
+trueCount = nameLower.count("t") + nameLower.count("r") + nameLower.count("u") + nameLower.count("e")
+# LOVE 的個數
+loveCount = nameLower.count("l") + nameLower.count("o") + nameLower.count("v") + nameLower.count("e")
+# 計算分數
+score = int(str(trueCount) + str(loveCount))
+
+if (score < 10) or (score > 90):
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}")
+    
+print("=======================")
+
+# Example - Treasure Island
+# 流程圖：https://app.diagrams.net/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+# https://ascii.co.uk/art
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+action = input("You're at a cross rood. Where do you want to go? Type \"left\" or \"right\" ")
+if action.lower() == "left":
+    action = input("You com to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim to swim across. ")
+    if action.lower() == "wait":
+        action = input("You arrive at the island unharmed. There is a hourse with 3 doors. One red, one yellow and one blue. Which color do you choose? ")
+        if action.lower() == "red":
+            print("Burned by fire. Game Over.")
+        elif action.lower() == "blue":
+            print("Eaten by beasts. Game Over.")
+        elif action.lower() == "yellow":
+            print("You found the treasure! You Win!")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by trout. Game Over.")
+else:
+    print("Fall into a hole. Game Over.")
