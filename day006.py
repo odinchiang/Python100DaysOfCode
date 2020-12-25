2d7dc87e51d40e7093a1d605d127e54957270779
Odin <odin.chiang@gmail.com>
2020-12-25 15:43:58 +0800

    day006 complete

---
+++ day006.py
@@ -0,0 +1,19 @@
+# Beginner - Python Functions & Karel
+
+# Built-in Functions
+# https://docs.python.org/3/library/functions.html
+
+# 定義函式
+def myFunction():
+    print("Hello")
+    print("Bye")
+    
+myFunction()
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# https://www.python.org/dev/peps/pep-0008/
+
+
+
