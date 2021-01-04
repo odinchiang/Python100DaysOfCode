d484e8ef813c0e40c08115687a5b929d83102676
Odin <odin.chiang@gmail.com>
2021-01-04 14:19:25 +0800

    重構-將Snake寫成類別

--- day020_Snake_Game.py
+++ day020_Snake_Game.py
@@ -1,5 +1,6 @@
 # Intermediate - Build the Snake Game
 
+from day020_Snake import Snake
 from turtle import Turtle, Screen
 import time
 
@@ -19,27 +20,14 @@ screen.tracer(0)
 # Detect collision with tail
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
 
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()  # 每個方塊都移動完再 update 視窗
     time.sleep(0.2)  # 控制每次 update 視窗的間隔時間
-
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-
-    segments[0].forward(20)
+    snake.move()
 
 
 
