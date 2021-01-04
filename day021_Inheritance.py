498b3bb71815ac1482fd4bf18087cae62cfc577c
Odin <odin.chiang@gmail.com>
2021-01-04 17:12:55 +0800

    day020, day021 complete

---
+++ day021_Inheritance.py
@@ -0,0 +1,25 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
+
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    # 複寫父類別的方法
+    def breathe(self):
+        super().breathe()
+        print("doing this upderwater.")
+
+    def swim(self):
+        print("moving in water.")
+
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
+print(nemo.num_eyes)
