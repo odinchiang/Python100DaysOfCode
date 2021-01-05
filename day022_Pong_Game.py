82937e70d285fe09272b1e1b45b01591d0c5e0c7
Odin <odin.chiang@gmail.com>
2021-01-05 11:30:20 +0800

    1. Detect collision with paddle 2. Detect when paddle misses

--- day022_Pong_Game.py
+++ day022_Pong_Game.py
@@ -47,6 +47,18 @@ while game_is_on:
 
     # Detect collision with wall and bounce
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
